b7c1bc6e98817e14d87f07b00c4cc6166f51af8c
Scott Bellware <sbellware@gmail.com>
2016-05-16 18:41:39 -0500

    Rudimentary implementation of shift

--- lib/clock/utc.rb
+++ lib/clock/utc.rb
@@ -6,6 +6,20 @@ module Clock
       Clock.utc(time)
     end
 
+    def self.shift(time)
+      if time.is_a? String
+        time = Time.parse(time)
+      end
+
+      offset = time.gmt_offset
+
+      utc_time = time.getutc
+
+      utc_time = utc_time + offset
+
+      now(utc_time)
+    end
+
     module Substitute
       def self.build
         Clock::Substitute.new
