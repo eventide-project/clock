385b2bfab0886c7bf654e60cc68f651ccff76414
Nathan Ladd <nathan@obsidianexchange.com>
2016-09-22 09:58:38 -0500

    Duplicate code is removed

--- lib/clock/controls/time/elapsed.rb
+++ lib/clock/controls/time/elapsed.rb
@@ -19,26 +19,6 @@ module Clock
             reference + elapsed_seconds
           end
         end
-
-        module Unit
-          module Frequency
-            def self.example(precision: nil)
-              precision ||= Clock::ISO8601::Defaults.precision
-
-              10 ** precision
-            end
-          end
-
-          module Duration
-            module Seconds
-              def self.example(precision: nil)
-                frequency = Frequency.example precision: precision
-
-                Rational(1, frequency)
-              end
-            end
-          end
-        end
       end
     end
   end
