b6eb3108e7303c422dfa009de5fcb78667c17af8
Nathan Ladd <nathan@obsidianexchange.com>
2016-09-22 12:42:42 -0500

    Frequency and Duration namespaces are removed

--- lib/clock/controls.rb
+++ lib/clock/controls.rb
@@ -1,3 +1,2 @@
 require 'clock/controls/time'
 require 'clock/controls/time/elapsed'
-require 'clock/controls/time/elapsed/unit'
--- lib/clock/controls/time/elapsed.rb
+++ lib/clock/controls/time/elapsed.rb
@@ -14,11 +14,21 @@ module Clock
 
             reference = Time::Raw.example
 
-            unit_duration = Unit::Duration::Seconds.example precision: precision
+            unit_duration = Unit.seconds precision: precision
 
             elapsed_seconds = units * unit_duration
 
             reference + elapsed_seconds
+          end
+        end
+
+        module Unit
+          def self.seconds(precision: nil)
+            precision ||= Clock::ISO8601::Defaults.precision
+
+            frequency = 10 ** precision
+
+            Rational(1, frequency)
           end
         end
       end
