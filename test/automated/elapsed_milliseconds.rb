f91b37899c316022bd9a4b2031707c3c7bdd1ecf
Scott Bellware <sbellware@gmail.com>
2016-09-22 21:07:06 -0500

    Automated, rather than bench

--- test/automated/elapsed_milliseconds.rb
+++ test/automated/elapsed_milliseconds.rb
@@ -1,4 +1,4 @@
-require_relative 'bench_init'
+require_relative 'automated_init'
 
 context "Clock" do
   context "Elapsed milliseconds" do
--- test/automated/substitute.rb
+++ test/automated/substitute.rb
@@ -1,4 +1,4 @@
-require_relative 'bench_init'
+require_relative 'automated_init'
 
 context "Substitute clock" do
   now = Time.parse("Jan 1 11:11:11 EDT 2000")
