21b2ad79a7b144c211f8d680f89d52d5bc8ba504
Scott Bellware <sbellware@gmail.com>
2025-03-13 19:30:18 -0700

    Substitute returns live system time if time is not explicitly controlled

--- lib/clock/substitute.rb
+++ lib/clock/substitute.rb
@@ -5,28 +5,17 @@ module Clock
     attr_writer :system_time
 
     def system_time
-      @system_time ||= self.class.null_time
+      @system_time ||= Clock.system_time
     end
 
     def now=(val)
-      system_time = OpenStruct.new
+      self.system_time = SubstituteSystemTime.new
       system_time.now = val
-      self.system_time = system_time
       system_time
     end
 
-    def iso8601(time=nil, precision: nil)
-      if system_time.is_a? OpenStruct
-        return super(time, precision: precision)
-      end
-      nil
-    end
-
-    def self.null_time
-      Mimic.(Object) do
-        def method_missing(*)
-        end
-      end
+    class SubstituteSystemTime
+      attr_accessor :now
     end
   end
 end
--- test/automated/substitute/substitute.rb
+++ test/automated/substitute/substitute.rb
@@ -1,38 +1,53 @@
 require_relative '../automated_init'
 
 context "Substitute clock" do
-  now = Clock::Controls::Time::Raw.example
-
-  clock_class = Clock::Substitute
-
   context "No assigned time" do
-    clock = clock_class.new
+    clock = Clock::Substitute.new
 
     context "Current Time" do
-      test "Is nil" do
-        assert(clock.now.nil?)
+      current_time = clock.now
+
+      comment current_time.inspect
+
+      test "Is a Time value" do
+        assert(current_time.is_a?(Time))
       end
     end
 
     context "ISO 8601" do
-      test "Time represented as a string" do
-        assert(clock.iso8601.nil?)
+      iso8601_now = clock.iso8601
+
+      comment iso8601_now.inspect
+
+      test "Is a String value" do
+        assert(clock.iso8601.is_a?(String))
       end
     end
   end
 
   context "Assigned Time" do
-    clock = clock_class.new
-    clock.now = now
+    clock = Clock::Substitute.new
+
+    control_time = Clock::Controls::Time::Raw.example
+
+    comment "Control: #{control_time.inspect}"
+
+    clock.now = control_time
 
     context "Current Time" do
+      current_time = clock.now
+
+      comment current_time.inspect
+
       test "Is the assigned time" do
-        assert(clock.now == now)
+        assert(clock.now == control_time)
       end
     end
 
     context "ISO 8601" do
-      iso8601_now = now.iso8601(3)
+      iso8601_now = control_time.iso8601(3)
+
+      comment iso8601_now.inspect
 
       test "Time represented as a string" do
         assert(clock.iso8601 == iso8601_now)
